8ab88b261e8bcb07720947fd4143c9be68ee4820
Frolian Ernest <84269100+FROLIANI@users.noreply.github.com>
2024-06-03 20:20:43 +0300

    fix(finalizesetup)

--- App.js
+++ App.js
@@ -1,16 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import LoginScreen from './App/Screen/LoginScreen';
+
 
 export default function App() {
-  
+
   const [fontsLoaded, fontError] = useFonts({
-    'Inter-Black': require('./assets/fonts/Inter-Black.otf'),
+    'outfit': require('./assets/fonts/Outfit-Regular.ttf'),
+    'outfit-bold': require('./assets/fonts/Outfit-Bold.ttf'),
+    'outfit-medium': require('./assets/fonts/Outfit-Medium.ttf'),
   });
 
   return (
     <View style={styles.container}>
-      <Text>Learn app Development</Text>
-      <StatusBar style="auto" />
+      {/* <Text style ={{fontFamily:'outfit'}}>
+        Learn app Development
+        </Text> */}
+      {/* <StatusBar style="auto" /> */}
+      <LoginScreen/>
     </View>
   );
 }
